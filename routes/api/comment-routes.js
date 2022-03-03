f46f5fb985febae20539e2cefe5bf1639ec719db
asdi80 <a8sadi@gmail.com>
2022-03-02 19:40:42 -0800

    comments and reply

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { addComment, removeComment,addReply, removeReply } = require('../../controllers/comment-controller');
+
+//api/comments/<pizzaId>
+router.route('/:pizzaId').post(addComment);
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+// /api/comments/<pizzaId>/<commentId>
+router.route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+module.exports = router; 
